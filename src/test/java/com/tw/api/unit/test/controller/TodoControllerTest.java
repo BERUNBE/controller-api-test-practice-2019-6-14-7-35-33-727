44bb7685e6ae82a151c5270f7aec7ecbf70b0b32
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-16 17:31:21 +0800

    Added test for TodoController saveTodo method

--- src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
+++ src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
@@ -1,14 +1,17 @@
 package com.tw.api.unit.test.controller;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.tw.api.unit.test.domain.todo.Todo;
 import com.tw.api.unit.test.domain.todo.TodoRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -17,7 +20,10 @@ import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -62,5 +68,26 @@ public class TodoControllerTest {
         result.andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.title").value("title"));
+    }
+
+    @Test
+    public void todoController_saveTodo_should_return_one_todo() throws Exception {
+        //given
+        Todo todo1 = new Todo(1, "title", true, 1);
+        String inputJson = mapToJson(todo1);
+
+        //when
+        ResultActions result = mvc.perform(post("/todos")
+                .contentType(APPLICATION_JSON)
+                .content(inputJson));
+
+        //then
+        result.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value("title"));
+    }
+
+    public String mapToJson(Object obj) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(obj);
     }
 }
