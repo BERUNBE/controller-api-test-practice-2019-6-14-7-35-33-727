3c5097a12534c67cf44c00d8dec87a63d51bd5a5
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-17 08:10:41 +0800

    Added tests for TodoController DELETE and PATCH methods

--- src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
+++ src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
@@ -3,15 +3,12 @@ package com.tw.api.unit.test.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.tw.api.unit.test.domain.todo.Todo;
 import com.tw.api.unit.test.domain.todo.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -21,9 +18,7 @@ import java.util.Optional;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -58,8 +53,8 @@ public class TodoControllerTest {
     @Test
     public void todoController_getTodo_should_return_one_todo() throws Exception {
         //given
-        Todo todo1 = new Todo(1, "title", true, 1);
-        when(todoRepository.findById(1)).thenReturn(Optional.of(todo1));
+        Todo todo = new Todo(1, "title", true, 1);
+        when(todoRepository.findById(1)).thenReturn(Optional.of(todo));
 
         //when
         ResultActions result = mvc.perform(get("/todos/{todo-id}", "1"));
@@ -85,6 +80,38 @@ public class TodoControllerTest {
         result.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.title").value("title"));
+    }
+
+    @Test
+    public void todoController_deleteOneTodo_should_update_one_todo_and_return_updated_todo() throws Exception {
+        //given
+        Todo todo = new Todo(1, "title", true, 1);
+        when(todoRepository.findById(1)).thenReturn(Optional.of(todo));
+
+        //when
+        ResultActions resultDelete = mvc.perform(delete("/todos/{todo-id}", "1"));
+
+        //then
+        resultDelete.andExpect(status().isOk());
+    }
+
+    @Test
+    public void todoController_updateTodo_should_update_one_todo_and_return_updated_todo() throws Exception {
+        //given
+        Todo oldTodo = new Todo(1, "oldtitle", true, 1);
+        when(todoRepository.findById(1)).thenReturn(Optional.of(oldTodo));
+        Todo updatedTodo = new Todo(1, "updatedTitle", true, 1);
+        String inputJson = mapToJson(updatedTodo);
+
+        //when
+        ResultActions result = mvc.perform(patch("/todos/{todo-id}", "1")
+                .contentType(APPLICATION_JSON)
+                .content(inputJson));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value("updatedTitle"));
     }
 
     public String mapToJson(Object obj) throws JsonProcessingException {
